b1b3ce8be1b180b71c28d0c843bb6b8121ffbf22
omer ram <0meram95@gmail.com>
2022-03-17 10:41:32 +0200

    constructing responsive

--- src/components/constructing/FooterElements.js
+++ src/components/constructing/FooterElements.js
@@ -1,39 +1,38 @@
 import styled from 'styled-components'
 
 export const FooterContainer = styled.div`
-background-color: #ffc400;
-height: 100vh
-`;
-
+  background-color: #ffc400;
+  height: 100vh;
+`
 
 export const FooterWrap = styled.div`
-padding: 88px 24px;
-justify-content: center;
-align-items: center;
-max-width: 1100px;
-margin: 0 auto;
-
-`;
+  padding: 88px 24px;
+  justify-content: center;
+  align-items: center;
+  max-width: 1100px;
+  margin: 0 auto;
+`
 
 export const SocialMediaWrap = styled.div`
-align-items: center;
-text-align: center;
-justify-content: center;
-max-width: 1100px;
-margin:  0 ;
-
-@media screen and (max-width: 820px) {
-}
-`;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  max-width: 1100px;
+  margin: 0;
+`
 
 export const NavLogo = styled.img`
-align-items: center;
-width: 400px;
-height: 220px;
+  align-items: center;
+  width: 400px;
+  height: 220px;
 
-&:hover{
+  &:hover {
     transform: scale(1.09);
     transition: all 0.2s ease-in-out;
     cursor: pointer;
-}
-`;
+  }
+  @media screen and (max-width: 520px) {
+    width: 250px;
+    height: 140px;
+  }
+`
